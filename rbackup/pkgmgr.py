d9f0559ee9c5c61ad1c33c2039f4dfbdc55b3913
Lex van Roon <r3boot@r3blog.nl>
2013-07-01 22:17:08 +0200

    Several fixes in the package manager code, making it actually work

--- rbackup/pkgmgr.py
+++ rbackup/pkgmgr.py
@@ -3,7 +3,7 @@ import os
 
 from rbackup import BaseClass
 
-class PackageManagers(BaseClass):
+class PackageManager(BaseClass):
     _package_managers = [   # Yes, this is incomplete
         'dpkg',
         'aptitude',
@@ -11,22 +11,23 @@ class PackageManagers(BaseClass):
         'rpm',
         'pacman',
         'pak',
-        'make install'
+        'makeinstall'
     ]
     def __init__(self, logger):
         BaseClass.__init__(self, logger)
+
+    def is_package_manager(self, cmdline):
+        for pkgmgr in self._package_managers:
+            if pkgmgr in cmdline:
+                return True
+        return False
 
     def has_running_pkgmgr(self):
         pids = [pid for pid in os.listdir('/proc') if pid.isdigit()]
 
         for pid in pids:
             cmdline = open(os.path.join('/proc', pid, 'cmdline'), 'rb').read()
-            if 'make install' in cmdline:
-                cmd = 'make install'
-            else:
-                cmd = os.path.basename(cmdline.split()[0])
-
-            if cmd in self._package_managers:
+            if self.is_package_manager(cmdline):
                 return True
 
         return False
